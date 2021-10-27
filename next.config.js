92846e0bb94d4e19111171fcd1440c977e8e80af
techntvz <techntvz@gmail.com>
2021-10-27 09:14:49 +0700

    update transpile

--- next.config.js
+++ next.config.js
@@ -1,39 +1,41 @@
 // next.config.js
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
-
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      inlineImageLimit: 8192,
-      imagesFolder: "images",
-      imagesName: "[name]-[hash].[ext]",
-      handleImages: ["jpeg", "png", "svg", "webp", "gif"],
-      removeOriginalExtension: false,
-      optimizeImages: true,
-      optimizeImagesInDev: false,
-      mozjpeg: {
-        quality: 80,
+const withTM = require("next-transpile-modules")(["next-optimized-images"]);
+module.exports = withTM(
+  withPlugins([
+    [
+      optimizedImages,
+      {
+        inlineImageLimit: 8192,
+        imagesFolder: "images",
+        imagesName: "[name]-[hash].[ext]",
+        handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+        removeOriginalExtension: false,
+        optimizeImages: true,
+        optimizeImagesInDev: false,
+        mozjpeg: {
+          quality: 80,
+        },
+        optipng: {
+          optimizationLevel: 3,
+        },
+        responsive: {
+          adapter: require("responsive-loader/sharp"),
+        },
+        pngquant: false,
+        gifsicle: {
+          interlaced: true,
+          optimizationLevel: 3,
+        },
+        svgo: {
+          // enable/disable svgo plugins here
+        },
+        webp: {
+          preset: "default",
+          quality: 75,
+        },
       },
-      optipng: {
-        optimizationLevel: 3,
-      },
-      responsive: {
-        adapter: require("responsive-loader/sharp"),
-      },
-      pngquant: false,
-      gifsicle: {
-        interlaced: true,
-        optimizationLevel: 3,
-      },
-      svgo: {
-        // enable/disable svgo plugins here
-      },
-      webp: {
-        preset: "default",
-        quality: 75,
-      },
-    },
-  ],
-]);
+    ],
+  ])
+);
